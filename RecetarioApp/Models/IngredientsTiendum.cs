d9b17bd1b2f5d97865888101fa0cb551bf6750a4
Andres Torres <santiago.ojedatorres@gmail.com>
2023-05-25 17:06:00 -0500

    Created new controllers

--- RecetarioApp/Models/IngredientsTiendum.cs
+++ RecetarioApp/Models/IngredientsTiendum.cs
@@ -5,6 +5,7 @@ namespace RecetarioApp.Models;
 
 public partial class IngredienteTiendum
 {
+    public int IdIngredientTiendum { get; set; }
     public int IdTienda { get; set; }
 
     public int IdIngrediente { get; set; }
